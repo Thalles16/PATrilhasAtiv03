32664e2925c0aee8c0964de7f1b6de59b889d4e1
Thalles <168739769+Thalles16@users.noreply.github.com>
2025-01-29 11:00:06 -0300

    questao i.cs

---
+++ questao i.cs
@@ -0,0 +1,32 @@
+using System;
+	
+	class program 
+{
+	static void Main ()
+	{ 
+		Console.WriteLine("escolha um numero");
+		int numero1 = int.Parse(Console.ReadLine());
+
+		Console.WriteLine("escolha um numero");
+		int numero2 = int .Parse(Console.ReadLine());
+		
+		int diferenca = numero1 - numero2;
+		
+		if ( diferenca <= 10 )
+		{
+			Console.WriteLine("os numeros tem diferença menor ou igual a 10");
+		}
+		else 
+		{
+			Console.WriteLine("os numeros tem diferença maior que 10");
+		}
+		
+		
+		
+		
+		Console.WriteLine("aperte qualquer tecla para fechar");
+		Console.ReadKey();
+    
+	}
+	}
+	
