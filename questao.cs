6e31fcf5b5aa9bdd45250c1083771637a25504c1
Thalles <168739769+Thalles16@users.noreply.github.com>
2025-01-29 10:34:52 -0300

    questao.cs

---
+++ questao.cs
@@ -0,0 +1,30 @@
+using System;
+	
+	class program 
+{
+	static void Main ()
+	{ 
+		Console.WriteLine("escolha um numero");
+		float numero1 = float.Parse(Console.ReadLine());
+
+		Console.WriteLine("escolha um numero");
+		float numero2 = float.Parse(Console.ReadLine());
+		
+		if (numero1 <= numero2)
+		{
+			Console.WriteLine("o primeiro numero é menor ou igual ao segundo");
+		}
+		else 
+		{
+			Console.WriteLine("o primeiro numero não é menor ou igual ao segundo");
+		}
+		
+		
+		
+		
+		Console.WriteLine("aperte qualquer tecla para fechar");
+		Console.ReadKey();
+    
+	}
+	}
+		
